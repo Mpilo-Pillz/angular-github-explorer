b9c0e510ec2eaee3c3a4d826ec078e43ba8375e3
mpilopillz <mpilo.dlamini@yahoo.com>
2024-07-21 08:47:26 +0200

    uc-1: WIP - fixing failing github service test

--- src/app/core/services/github.service.spec.ts
+++ src/app/core/services/github.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { GithubService } from './github.service';
 
 describe('GithubService', () => {
   let service: GithubService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GithubService],
+    });
     service = TestBed.inject(GithubService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
