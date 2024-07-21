487617adebfa390c6b6435150d0881d230a1fe94
mpilopillz <mpilo.dlamini@yahoo.com>
2024-07-21 12:19:46 +0200

    UC-1: fetching repositories and logging to console. Using correct types

--- src/app/features/repositories/components/repository-list/repository-list.component.ts
+++ src/app/features/repositories/components/repository-list/repository-list.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { GithubService } from '../../../../core/services/github.service';
+import { Repository } from '../../../../core/models/repository.model';
 
 @Component({
   selector: 'app-repository-list',
   templateUrl: './repository-list.component.html',
 })
 export class RepositoryListComponent {
+  repositories: Repository[];
+  constructor(private githubService: GithubService) {
+    this.repositories = [];
+  }
   onSearch(query: string) {
-    console.log(query);
+    this.githubService
+      .searchRepositories(query)
+      .subscribe((repositoryResponse) => {
+        console.log(repositoryResponse);
+      });
   }
 }
