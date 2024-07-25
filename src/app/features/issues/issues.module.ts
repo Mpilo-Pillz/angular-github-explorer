d5b4a64b27af44c13af642fd1b4aca06c20dddde
mpilopillz <mpilo.dlamini@yahoo.com>
2024-07-25 18:14:02 +0200

    uc-5: using color alias

--- src/app/features/issues/issues.module.ts
+++ src/app/features/issues/issues.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IssuesListComponent } from './components/issues-list/issues-list.component';
 import { IssuesRoutingModule } from './issues-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [IssuesListComponent],
-  imports: [CommonModule, IssuesRoutingModule],
+  imports: [CommonModule, IssuesRoutingModule, SharedModule],
 })
 export class IssuesModule {}
