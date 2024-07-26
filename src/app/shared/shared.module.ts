687f4202e1d17dfe25d0850a688684d702e0ed66
mpilopillz <mpilo.dlamini@yahoo.com>
2024-07-26 02:54:49 +0200

    removing unused component on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './components/button/button.component';
+import { NoResultsComponent } from './components/no-results/no-results.component';
 import { RepoIconComponent } from './components/repo-icon/repo-icon.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { TextInputComponent } from './components/text-input/text-input.component';
-import { NoResultsComponent } from './components/no-results/no-results.component';
 
 @NgModule({
   declarations: [
     SearchBarComponent,
     RepoIconComponent,
     ButtonComponent,
-    TextInputComponent,
     NoResultsComponent,
   ],
   imports: [CommonModule, FormsModule, HttpClientModule],
