ef7e7a62ab0009566346bae9152bf327b32bb1d6
mpilopillz <mpilo.dlamini@yahoo.com>
2024-07-26 05:34:56 +0200

    td-3: Updating docs and cleaning up subscriptions

--- src/app/features/repositories/components/repository-list/repository-list.component.ts
+++ src/app/features/repositories/components/repository-list/repository-list.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { GithubService } from '../../../../core/services/github.service';
 import { Repository } from '../../../../core/models/repository.model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-repository-list',
   templateUrl: './repository-list.component.html',
 })
-export class RepositoryListComponent {
+export class RepositoryListComponent implements OnDestroy {
+  hasSearched = false;
+  private destroy$ = new Subject<void>();
   repositories: Repository[];
+  loading: boolean = false;
   constructor(private githubService: GithubService) {
     this.repositories = [];
   }
   onSearch(query: string) {
+    this.loading = true;
+    this.hasSearched = true;
     this.githubService
       .searchRepositories(query)
-      .subscribe((repositoryResponse) => {
-        console.log(repositoryResponse);
-        this.repositories = repositoryResponse.items;
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (repositoryResponse) => {
+          this.repositories = repositoryResponse.items;
+          this.loading = false;
+        },
+        error: (error) => {
+          alert(error);
+          this.loading = false;
+        },
       });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
